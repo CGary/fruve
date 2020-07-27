4420466d9d8de3a08e4c4980017a654f2b6feb96
Cristian Nuñez Optiplex9020 Prosur <cnunez@via-prosur.com>
2020-07-27 08:52:43 -0400

    Se creo el proyecto

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: ["@babel/polyfill", path.resolve(__dirname, "src/index.js")],
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[hash].js",
+    chunkFilename: "js/[id].[name].[chunkhash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[hash].[ext]",
+            outputPath: "assets/",
+          },
+        },
+      },
+    ],
+  },
+};
---
+++ webpack.dev.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: ["@babel/polyfill", path.resolve(__dirname, "src/index.js")]
+  },
+  mode: "development",
+  output: {
+    filename: "[name].js",
+    publicPath: "/",
+    chunkFilename: "[id].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html")
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    // open: true,
+    port: 9001,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/,
+        use: 'file-loader'
+      }
+    ]
+  }
+};
